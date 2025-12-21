34650519d9077cc354a09ddcfdee98e85c08d28a
karthikeya-dev <142624110+Karthikeya72@users.noreply.github.com>
2025-12-21 13:30:37 +0530

    Integration and data displaying

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectService } from './services/project';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('realestate-frontend');
+export class App implements OnInit {
+
+  projects: any[] = [];
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projectService.getProjects().subscribe(data => {
+      this.projects = data;
+    });
+  }
 }
---
+++ src/app/services/project.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  private apiUrl = 'http://localhost:8080/api/projects';
+
+  constructor(private http: HttpClient) {}
+
+  getProjects() {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
